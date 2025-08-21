2f4f4955171fb80350776366c447c20415234dac
asadomeio <asadomeiowow@gmail.com>
2025-08-21 09:43:29 -0400

    Create notable-packs.js

---
+++ js/packs/notable-packs.js
@@ -0,0 +1,30 @@
+export const notablePacks = [
+  {
+    title: "Wrath of the Lich King: Raid Mounts",
+    author: "Community",
+    icon: "https://wow.zamimg.com/images/wow/icons/large/inv_mount_lichkinghorse.jpg",
+    importCode: "eJwrzs8tL85Jzs8rSc0rsNUz1DNQ0FfUM1DSUdA11DNUM1A31TNUM1BQ1DNUM1A31zNQN1Q11zNQtVQrS80pTS4pTc4vLUpNLS7JzM9T0uOSzExtKUrNSSxJLQYpYKVgZWBkZKRubW5hZWFoZmRiamhuaWRoDOrA0pKbWJpDOrA0tbSyhB2sYJtYgY3yWJItE8sHACY8M0c=",
+    items: [
+      { name: "Invincible's Reins", itemID: 50818, chance: 100, npcs: [{ id: 36597 }] },
+      { name: "Mimiron's Head", itemID: 45693, chance: 100, npcs: [{ id: 33288 }] },
+      { name: "Onyxian Drake", itemID: 49636, chance: 100, npcs: [{ id: 10184 }] },
+      { name: "Grand Black War Mammoth", itemID: 44083, chance: 100, npcs: [{ id: 32669 }, { id: 32652 }, { id: 32670 }, { id: 32667 }] },
+      { name: "Azure Drake", itemID: 43952, chance: 25, npcs: [{ id: 28859 }] },
+      { name: "Blue Drake", itemID: 43953, chance: 25, npcs: [{ id: 28859 }] }
+    ]
+  },
+  {
+    title: "Classic Dungeon & Raid Pets",
+    author: "Community",
+    icon: "https://wow.zamimg.com/images/wow/icons/large/inv_pet_elemental_moltencore.jpg",
+    importCode: "eJwrzs8tL85Jzs8rSc0rsNUz1DNQ0FfUM1DSUdA11DNQN1TTM1A31DNQN1M1Ndc11zNQN1Qz1zNQN1Uz0NNYLSvNKaXJJSkpT84vLUpNLS7JzM9T0uMSzAx1KUrNSSxJLQYpYKVgZWBkZKRubW5hZWFoZmRiamhuaWRoDOrA0pKbWJpDOrA0tbSyhB2sYJtYgY2iWJKNA0uYk1iaQzqwtLaWkh2ssG1hBRuYk1gyMLeGDBRGLgYA51E7sg==",
+    items: [
+      { name: "Mojo", itemID: 8642, chance: 50, npcs: [{ id: 11520 }] },
+      { name: "Worg Pup", itemID: 29362, chance: 25, npcs: [{ id: 11467 }] },
+      { name: "Smolderweb Hatchling", itemID: 11473, chance: 10, npcs: [{ id: 10444 }] },
+      { name: "Core Hound Pup", itemID: 118237, chance: 1, npcs: [{ id: 11671 }] },
+      { name: "The Beak", itemID: 11499, chance: 20, npcs: [{ id: 10509 }, { id: 10507 }, { id: 10506 }, { id: 10508 }] },
+      { name: "Dark Whelpling", itemID: 10822, chance: 100, npcs: [{ id: 9662 }, { id: 9663 }, { id: 9664 }] }
+    ]
+  }
+];
