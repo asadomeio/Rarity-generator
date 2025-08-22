36e1376c669323f89c8cda0078dbbae36fe9db2d
asadomeio <asadomeiowow@gmail.com>
2025-08-22 09:06:50 -0400

    Update notable-packs.js

--- js/packs/notable-packs.js
+++ js/packs/notable-packs.js
@@ -5,26 +5,25 @@ export const notablePacks = [
     icon: "https://wow.zamimg.com/images/wow/icons/large/inv_mount_lichkinghorse.jpg",
     importCode: "eJwrzs8tL85Jzs8rSc0rsNUz1DNQ0FfUM1DSUdA11DNUM1A31TNUM1BQ1DNUM1A31zNQN1Q11zNQtVQrS80pTS4pTc4vLUpNLS7JzM9T0uOSzExtKUrNSSxJLQYpYKVgZWBkZKRubW5hZWFoZmRiamhuaWRoDOrA0pKbWJpDOrA0tbSyhB2sYJtYgY3yWJItE8sHACY8M0c=",
     items: [
-      { name: "Invincible's Reins", itemID: 50818, chance: 100, npcs: [{ id: 36597 }] },
-      { name: "Mimiron's Head", itemID: 45693, chance: 100, npcs: [{ id: 33288 }] },
-      { name: "Onyxian Drake", itemID: 49636, chance: 100, npcs: [{ id: 10184 }] },
-      { name: "Grand Black War Mammoth", itemID: 44083, chance: 100, npcs: [{ id: 32669 }, { id: 32652 }, { id: 32670 }, { id: 32667 }] },
-      { name: "Azure Drake", itemID: 43952, chance: 25, npcs: [{ id: 28859 }] },
-      { name: "Blue Drake", itemID: 43953, chance: 25, npcs: [{ id: 28859 }] }
+      { type: "MOUNT", name: "Invincible's Reins", itemID: 50818, chance: 100, npcs: [{ id: 36597, name: "The Lich King" }] },
+      { type: "MOUNT", name: "Mimiron's Head", itemID: 45693, chance: 100, npcs: [{ id: 33288, name: "Yogg-Saron" }] },
+      { type: "MOUNT", name: "Onyxian Drake", itemID: 49636, chance: 100, npcs: [{ id: 10184, name: "Onyxia" }] },
+      { type: "MOUNT", name: "Azure Drake", itemID: 43952, chance: 25, npcs: [{ id: 28859, name: "Malygos" }] },
+      { type: "MOUNT", name: "Blue Drake", itemID: 43953, chance: 25, npcs: [{ id: 28859, name: "Malygos" }] }
     ]
   },
   {
-    title: "Classic Dungeon & Raid Pets",
+    title: "Cataclysm: Raid Mounts",
     author: "Community",
-    icon: "https://wow.zamimg.com/images/wow/icons/large/inv_pet_elemental_moltencore.jpg",
-    importCode: "eJwrzs8tL85Jzs8rSc0rsNUz1DNQ0FfUM1DSUdA11DNQN1TTM1A31DNQN1M1Ndc11zNQN1Qz1zNQN1Uz0NNYLSvNKaXJJSkpT84vLUpNLS7JzM9T0uMSzAx1KUrNSSxJLQYpYKVgZWBkZKRubW5hZWFoZmRiamhuaWRoDOrA0pKbWJpDOrA0tbSyhB2sYJtYgY2iWJKNA0uYk1iaQzqwtLaWkh2ssG1hBRuYk1gyMLeGDBRGLgYA51E7sg==",
+    icon: "https://wow.zamimg.com/images/wow/icons/large/inv_mount_firehawk.jpg",
+    importCode: "eJwrzs8tL85Jzs8rSc0rsNUz1DNQ0FfUM1DSUdAz0jNQN9Qz1DNQN9Qz0NNQ0FfUM9Az0DVWNdc11zNQtVQrS80pTS4pTc4vLUpNLS7JzM9T0uMSzAx1KUrNSSxJLQYpYKVgZWBkZKRubW5hZWFoZmRiamhuaWRoDOrA0pKbWJpDOrA0tbSyhB2sYJtYgY3yWJItE8sHACY8M0c=",
     items: [
-      { name: "Mojo", itemID: 8642, chance: 50, npcs: [{ id: 11520 }] },
-      { name: "Worg Pup", itemID: 29362, chance: 25, npcs: [{ id: 11467 }] },
-      { name: "Smolderweb Hatchling", itemID: 11473, chance: 10, npcs: [{ id: 10444 }] },
-      { name: "Core Hound Pup", itemID: 118237, chance: 1, npcs: [{ id: 11671 }] },
-      { name: "The Beak", itemID: 11499, chance: 20, npcs: [{ id: 10509 }, { id: 10507 }, { id: 10506 }, { id: 10508 }] },
-      { name: "Dark Whelpling", itemID: 10822, chance: 100, npcs: [{ id: 9662 }, { id: 9663 }, { id: 9664 }] }
+      { type: "MOUNT", name: "Flametalon of A'lysrazor", itemID: 71665, chance: 100, npcs: [{ id: 52530, name: "Alysrazor" }] },
+      { type: "MOUNT", name: "Smoldering Egg of Millagazor", itemID: 71700, chance: 100, npcs: [{ id: 52409, name: "Ragnaros" }] },
+      { type: "MOUNT", name: "Reins of the Blazing Drake", itemID: 77069, chance: 100, npcs: [{ id: 56173, name: "Madness of Deathwing" }] },
+      { type: "MOUNT", name: "Life-Binder's Handmaiden", itemID: 77068, chance: 100, npcs: [{ id: 56173, name: "Madness of Deathwing" }] },
+      { type: "MOUNT", name: "Experiment 12-B", itemID: 77078, chance: 100, npcs: [{ id: 55294, name: "Ultraxion" }] },
+      { type: "MOUNT", name: "Reins of the Drake of the South Wind", itemID: 63040, chance: 100, npcs: [{ id: 46761, name: "Al'Akir" }] }
     ]
   }
 ];
