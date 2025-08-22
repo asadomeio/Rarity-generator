5a3c84177bd83f64ca7dbb4c3cf3d236f277708e
asadomeio <asadomeiowow@gmail.com>
2025-08-22 10:28:51 -0400

    Update notable-packs.js

--- js/packs/notable-packs.js
+++ js/packs/notable-packs.js
@@ -5,11 +5,11 @@ export const notablePacks = [
     icon: "https://wow.zamimg.com/images/wow/icons/large/inv_mount_lichkinghorse.jpg",
     importCode: "eJwrzs8tL85Jzs8rSc0rsNUz1DNQ0FfUM1DSUdA11DNUM1A31TNUM1BQ1DNUM1A31zNQN1Q11zNQtVQrS80pTS4pTc4vLUpNLS7JzM9T0uOSzExtKUrNSSxJLQYpYKVgZWBkZKRubW5hZWFoZmRiamhuaWRoDOrA0pKbWJpDOrA0tbSyhB2sYJtYgY3yWJItE8sHACY8M0c=",
     items: [
-      { type: "MOUNT", name: "Invincible's Reins", itemID: 50818, chance: 100, npcs: [{ id: 36597, name: "The Lich King" }] },
-      { type: "MOUNT", name: "Mimiron's Head", itemID: 45693, chance: 100, npcs: [{ id: 33288, name: "Yogg-Saron" }] },
-      { type: "MOUNT", name: "Onyxian Drake", itemID: 49636, chance: 100, npcs: [{ id: 10184, name: "Onyxia" }] },
-      { type: "MOUNT", name: "Azure Drake", itemID: 43952, chance: 25, npcs: [{ id: 28859, name: "Malygos" }] },
-      { type: "MOUNT", name: "Blue Drake", itemID: 43953, chance: 25, npcs: [{ id: 28859, name: "Malygos" }] }
+      { type: "MOUNT", name: "Invincible's Reins", itemID: 50818, chance: 100, npcIDs: [36597] },
+      { type: "MOUNT", name: "Mimiron's Head", itemID: 45693, chance: 100, npcIDs: [33288] },
+      { type: "MOUNT", name: "Onyxian Drake", itemID: 49636, chance: 100, npcIDs: [10184] },
+      { type: "MOUNT", name: "Azure Drake", itemID: 43952, chance: 25, npcIDs: [28859] },
+      { type: "MOUNT", name: "Blue Drake", itemID: 43953, chance: 25, npcIDs: [28859] }
     ]
   },
   {
@@ -18,12 +18,12 @@ export const notablePacks = [
     icon: "https://wow.zamimg.com/images/wow/icons/large/inv_mount_firehawk.jpg",
     importCode: "eJwrzs8tL85Jzs8rSc0rsNUz1DNQ0FfUM1DSUdAz0jNQN9Qz1DNQN9Qz0NNQ0FfUM9Az0DVWNdc11zNQtVQrS80pTS4pTc4vLUpNLS7JzM9T0uMSzAx1KUrNSSxJLQYpYKVgZWBkZKRubW5hZWFoZmRiamhuaWRoDOrA0pKbWJpDOrA0tbSyhB2sYJtYgY3yWJItE8sHACY8M0c=",
     items: [
-      { type: "MOUNT", name: "Flametalon of A'lysrazor", itemID: 71665, chance: 100, npcs: [{ id: 52530, name: "Alysrazor" }] },
-      { type: "MOUNT", name: "Smoldering Egg of Millagazor", itemID: 71700, chance: 100, npcs: [{ id: 52409, name: "Ragnaros" }] },
-      { type: "MOUNT", name: "Reins of the Blazing Drake", itemID: 77069, chance: 100, npcs: [{ id: 56173, name: "Madness of Deathwing" }] },
-      { type: "MOUNT", name: "Life-Binder's Handmaiden", itemID: 77068, chance: 100, npcs: [{ id: 56173, name: "Madness of Deathwing" }] },
-      { type: "MOUNT", name: "Experiment 12-B", itemID: 77078, chance: 100, npcs: [{ id: 55294, name: "Ultraxion" }] },
-      { type: "MOUNT", name: "Reins of the Drake of the South Wind", itemID: 63040, chance: 100, npcs: [{ id: 46761, name: "Al'Akir" }] }
+      { type: "MOUNT", name: "Flametalon of A'lysrazor", itemID: 71665, chance: 100, npcIDs: [52530] },
+      { type: "MOUNT", name: "Smoldering Egg of Millagazor", itemID: 71700, chance: 100, npcIDs: [52409] },
+      { type: "MOUNT", name: "Reins of the Blazing Drake", itemID: 77069, chance: 100, npcIDs: [56173] },
+      { type: "MOUNT", name: "Life-Binder's Handmaiden", itemID: 77068, chance: 100, npcIDs: [56173] },
+      { type: "MOUNT", name: "Experiment 12-B", itemID: 77078, chance: 100, npcIDs: [55294] },
+      { type: "MOUNT", name: "Reins of the Drake of the South Wind", itemID: 63040, chance: 100, npcIDs: [46761] }
     ]
   }
 ];
